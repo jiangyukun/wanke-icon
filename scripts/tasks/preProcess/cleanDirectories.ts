401e0360259cadb7743fb1e6ee3cb2f59492cb8e
jiangyukun <187_6810_5877@sina.com>
2020-06-14 16:36:58 +0800

    update

--- scripts/tasks/preProcess/cleanDirectories.ts
+++ scripts/tasks/preProcess/cleanDirectories.ts
@@ -1,12 +1,10 @@
 import del from 'del';
 
 export default function cleanDirectories() {
-  return del([
-    '../src',
-    'inline-svg',
-    'docs/inline-svg',
-    'docs/list.md',
-    'es',
-    'lib'
-  ], {force: true});
+    return del([
+        '../src/base',
+        '../es',
+        '../lib',
+        '../inline-svg',
+    ], {force: true});
 }
