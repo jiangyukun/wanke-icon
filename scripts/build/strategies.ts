a233a4ad4309a6e3054aee52df7db0138b4c4ab7
jiangyukun <187_6810_5877@sina.com>
2020-08-20 15:59:00 +0800

    添加栈端icon

--- scripts/build/strategies.ts
+++ scripts/build/strategies.ts
@@ -53,6 +53,7 @@ export function iconsAfter3Dot9ShouldBeResizeViewbox({
     && iconName.indexOf('wanke') == -1
     && iconName.indexOf('gf') == -1
     && iconName.indexOf('fz') == -1
+    && iconName.indexOf('zd') == -1
   ) {
     // make it immutable!
     abstractNode = {
